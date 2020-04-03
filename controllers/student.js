82a93f683d8f2f5b28a44259f4b84f37616e6872
dongpp <phuongdongIOT@gmail.com>
2020-04-03 16:31:25 +0700

    add CRUD

--- controllers/student.js
+++ controllers/student.js
@@ -4,7 +4,12 @@ const path = require('path')
 const bd = require('../helpers/body_parsers')
 var dt = require('../datas/data_students')
 var dataStudent = dt.students;
-
+var maxId = 0;
+for (var i = 0; i < dt.students.length; i++) {
+  if (dt.students[i].id > maxId) {
+    maxId = dt.students[i].id;
+  }
+}
 const filePathStudent = path.join(__dirname, '../views/student.html');
 
 exports.getViewStudents = (response) => {
@@ -37,15 +42,45 @@ exports.getDataOneStudent = (request, response) => {
   })
   var arrIndexUrl = request.url.split("/");
   for (var i = 0; i < dataStudent.length - 1; i++) {
-    if (dataStudent[i].id = arrIndexUrl[2]) {
+    if (dataStudent[i].id == arrIndexUrl[2]) {
       const jsonData = JSON.stringify(dataStudent[i])
       response.end(jsonData)
     }
   }
 }
 
-exports.updateOneStudents = async (request, response) => {
+exports.addOneStudent = async (request, response) => {
+  let requestBody = await bd.bodyParser(request)
+  const idStudent = requestBody[1].split(/=/)
+  const name = requestBody[2].split(/=/)
+  const age = requestBody[3].split(/=/)
+  const classs = requestBody[4].split(/=/)
+  const jsonData = {
+    id: ++maxId,
+    name: name[1],
+    age: age[1],
+    class: classs[1],
+  }
+  dataStudent.push(jsonData)
+  response.writeHead(302, {
+    'Location': 'http://localhost:9000/students'
+  });
+  response.end();
+}
 
+exports.updateOneStudents = async (request, response) => {
+  let requestBody = await bd.bodyParser(request)
+  const idStudent = requestBody[1].split(/=/)
+  const name = requestBody[2].split(/=/)
+  const age = requestBody[3].split(/=/)
+  const classs = requestBody[4].split(/=/)
+  for (let i = 0; i < dataStudent.length; i++) {
+    if (idStudent[1] == dataStudent[i].id) {
+      dataStudent[i].name = name[1];
+      dataStudent[i].age = age[1];
+      dataStudent[i].classs = classs[1];
+    }
+  }
 
   response.writeHead(302, {
     'Location': 'http://localhost:9000/students'
@@ -53,8 +88,14 @@ exports.updateOneStudents = async (request, response) => {
   response.end();
 }
 
-exports.deleteOneStudents = (request, response) => {
-  dataStudent.splice(2, 1);
+exports.deleteOneStudents = async (request, response) => {
+  let requestBody = await bd.bodyParser(request)
+  const idStudent = requestBody[1].split(/=/)
+  for (var i = 0; i < dataStudent.length; i++) {
+    if (idStudent[1] == dataStudent[i].id) {
+      console.log(dataStudent.splice(i, 1))
+    }
+  }
   response.writeHead(302, {
     'Location': 'http://localhost:9000/students'
   });
--- helpers/body_parsers.js
+++ helpers/body_parsers.js
@@ -1,6 +1,7 @@
 exports.bodyParser = async (request) => {
   return new Promise((resolve, reject) => {
     let totalChunked = ""
+    let arrtotalChunked = []
     request
       .on("error", err => {
         console.error(err)
@@ -10,8 +11,8 @@ exports.bodyParser = async (request) => {
         totalChunked += chunk
       })
       .on("end", () => {
-        request.body = totalChunked
-        resolve()
+        arrtotalChunked = totalChunked.split(/&/)
+        resolve(arrtotalChunked)
       })
   })
 }
